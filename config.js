2b4277eee3911f5bf34b18afe20815eda1333ec6
Nyohi <73756241+jeverson34@users.noreply.github.com>
2020-12-24 00:47:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-exports.Token = "NzgyNjQxMTc3NDA4MDQ1MDcw.X8PJZQ.EhfP9UFhQ-L-8HpdQWDzFCi8HQI"; //Bot Token - Important
-exports.Owner = "583794054240665601"; //Bot Owner ID - Important
+exports.Token = ""; //Bot Token - Important
+exports.Owner = ""; //Bot Owner ID - Important
 exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
-exports.Color = "#000cff"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
+exports.Color = ""; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
 exports.Support = `https://discord.gg/HD8EDFm`; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
 exports.Donate = `https://www.paypal.com/paypalme/legendemoji`; //Please Don't Change It, If You Want To Support Me <3
